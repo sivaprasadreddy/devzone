4c1013fa2739ea17a02cda2abacd220bdf67e1c3
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2020-11-08 09:55:43 +0530

    Move properties files into config folder, add sonar-project.properties

--- src/main/java/com/sivalabs/devzone/Application.java
+++ src/main/java/com/sivalabs/devzone/Application.java
@@ -1,20 +1,49 @@
 package com.sivalabs.devzone;
 
 import com.sivalabs.devzone.config.ApplicationProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
 @EnableAspectJAutoProxy
 @EnableCaching
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
     }
 
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n--------------------------------------------------------------\n" +
+                "\tApplication '{}' is running at {}://{}:{}{}\n" +
+                "\tProfile(s): {}\n--------------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol, hostAddress, serverPort, contextPath,
+            env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles());
+    }
 }
