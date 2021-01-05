16aab6e3fbe9648118243a86b1dbbaaf180744de
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2021-01-05 15:01:47 +0530

    Upgrade Swagger config

--- src/main/java/com/sivalabs/devzone/config/SwaggerConfig.java
+++ src/main/java/com/sivalabs/devzone/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.sivalabs.devzone.config;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -8,12 +9,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
