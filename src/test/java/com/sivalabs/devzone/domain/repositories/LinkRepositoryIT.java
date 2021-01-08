c4e9c3a3444c640bcea27f19e8bc060a178916d0
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2021-01-08 11:39:17 +0530

    Adding kubernetes deployment config

--- src/test/java/com/sivalabs/devzone/domain/repositories/LinkRepositoryIT.java
+++ src/test/java/com/sivalabs/devzone/domain/repositories/LinkRepositoryIT.java
@@ -3,21 +3,20 @@ package com.sivalabs.devzone.domain.repositories;
 import static com.sivalabs.devzone.utils.TestConstants.PROFILE_IT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.sivalabs.devzone.common.TestContainersInitializer;
 import com.sivalabs.devzone.domain.entities.Link;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@Slf4j
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=NONE",
+            "spring.datasource.url=jdbc:tc:postgresql:12.3:///testdb"
+        })
 @ActiveProfiles(PROFILE_IT)
-class LinkRepositoryIT extends TestContainersInitializer {
+class LinkRepositoryIT {
 
     @Autowired private LinkRepository linkRepository;
 
