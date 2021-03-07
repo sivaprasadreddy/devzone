cf66def74aa6eff7cd68f59c6976894c9288db52
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2021-03-07 11:51:35 +0530

    Apply google java format

--- src/main/java/com/sivalabs/devzone/domain/services/UserService.java
+++ src/main/java/com/sivalabs/devzone/domain/services/UserService.java
@@ -2,12 +2,11 @@ package com.sivalabs.devzone.domain.services;
 
 import com.sivalabs.devzone.domain.entities.User;
 import com.sivalabs.devzone.domain.repositories.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,5 +19,4 @@ public class UserService {
     public Optional<User> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
 }
