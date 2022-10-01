10e372e798dbfa5a2189bd610f222ac527b51c79
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-10-01 07:24:26 +0530

    Gatling config improvements

--- gatling-tests/src/gatling/java/devzone/LinkCreationSimulation.java
+++ gatling-tests/src/gatling/java/devzone/LinkCreationSimulation.java
@@ -5,20 +5,17 @@ import io.gatling.javaapi.core.FeederBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
+import utils.SimulationHelper;
 
 import java.time.Duration;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static utils.SimulationHelper.getConfig;
 
 public class LinkCreationSimulation extends Simulation {
 
-    HttpProtocolBuilder httpProtocol =
-        http.baseUrl("http://localhost:8080")
-            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-            .acceptLanguageHeader("en-US,en;q=0.5")
-            .acceptEncodingHeader("gzip, deflate")
-            .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0");
+    HttpProtocolBuilder httpProtocol = SimulationHelper.getHttpProtocolBuilder();
 
     FeederBuilder<String> linkFeeder = csv("data/feeders/links.csv").random();
     FeederBuilder<String> credentialsFeeder = csv("data/feeders/credentials.csv").random();
@@ -62,11 +59,11 @@ public class LinkCreationSimulation extends Simulation {
 
     {
         setUp(
-            scnCreateLink.injectOpen(rampUsers(10).during(10))
+            scnCreateLink.injectOpen(rampUsers(getConfig().getInt("users")).during(10))
         )
         .protocols(httpProtocol)
             .assertions(
-                global().responseTime().max().lt(2000),
+                global().responseTime().max().lt(800),
                 global().successfulRequests().percent().is(100.0)
             );
     }
